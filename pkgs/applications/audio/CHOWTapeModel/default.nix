54be1036615a7c9d2672d67a2d9b95d8ed1d012a
Bart Brouns <bart@magnetophon.nl>
2021-09-05 09:07:13 +0200

    Chowtapemodel: unstable-2020-12-12 -> 2.8.0

--- pkgs/applications/audio/CHOWTapeModel/default.nix
+++ pkgs/applications/audio/CHOWTapeModel/default.nix
@@ -1,62 +1,106 @@
 { alsa-lib
+, at-spi2-core
+, cmake
 , curl
+, dbus
+, epoxy
 , fetchFromGitHub
 , freeglut
 , freetype
+, gtk3
+, lib
 , libGL
 , libXcursor
+, libXdmcp
 , libXext
 , libXinerama
 , libXrandr
+, libXtst
+, libdatrie
 , libjack2
+, libpsl
+, libselinux
+, libsepol
+, libsysprof-capture
+, libthai
+, libxkbcommon
+, pcre
 , pkg-config
 , python3
+, sqlite
 , stdenv
-, lib
+, util-linuxMinimal
+, webkitgtk
+, gcc-unwrapped
 }:
 
 stdenv.mkDerivation rec {
   pname = "CHOWTapeModel";
-  version = "unstable-2020-12-12";
+  version = "2.8.0";
 
   src = fetchFromGitHub {
     owner = "jatinchowdhury18";
     repo = "AnalogTapeModel";
-    rev = "a7cf10c3f790d306ce5743bb731e4bc2c1230d70";
-    sha256 = "09nq8x2dwabncbp039dqm1brzcz55zg9kpxd4p5348xlaz5m4661";
+    rev = "v${version}";
+    sha256 = "0rl04hr6gpqdkjvvvxqv4qiqrwjd0gmc9nlpqmdjdlbgm195jmzv";
     fetchSubmodules = true;
   };
 
   nativeBuildInputs = [
     pkg-config
+    cmake
   ];
 
   buildInputs = [
     alsa-lib
+    at-spi2-core
     curl
+    dbus
+    epoxy
     freeglut
     freetype
+    gtk3
     libGL
     libXcursor
+    libXdmcp
     libXext
     libXinerama
     libXrandr
+    libXtst
+    libdatrie
     libjack2
+    libpsl
+    libselinux
+    libsepol
+    libsysprof-capture
+    libthai
+    libxkbcommon
+    pcre
     python3
+    sqlite
+    # util-linuxMinimal
+    # webkitgtk
+    gcc-unwrapped
   ];
 
-  buildPhase = ''
-    cd Plugin/
-    ./build_linux.sh
-  '';
+  cmakeFlags = [ "-DCMAKE_AR=${gcc-unwrapped}/bin/gcc-ar"
+                 "-DCMAKE_RANLIB=${gcc-unwrapped}/bin/gcc-ranlib"
+                 "-DCMAKE_NM=${gcc-unwrapped}/bin/gcc-nm" ];
+
+  postPatch = "cd Plugin";
+
+  # buildPhase = ''
+  #   cmake -Bbuild
+  #   cmake --build build/ --config Release
+  # '';
 
   installPhase = ''
     mkdir -p $out/lib/lv2 $out/lib/vst3 $out/bin $out/share/doc/CHOWTapeModel/
-    cd Builds/LinuxMakefile/build/
-    cp CHOWTapeModel.a  $out/lib
-    cp -r CHOWTapeModel.lv2 $out/lib/lv2
-    cp -r CHOWTapeModel.vst3 $out/lib/vst3
-    cp CHOWTapeModel  $out/bin
+    cd CHOWTapeModel_artefacts/Release
+    cp libCHOWTapeModel_SharedCode.a  $out/lib
+    cp -r LV2/CHOWTapeModel.lv2 $out/lib/lv2
+    cp -r VST3/CHOWTapeModel.vst3 $out/lib/vst3
+    cp Standalone/CHOWTapeModel  $out/bin
     cp ../../../../Manual/ChowTapeManual.pdf $out/share/doc/CHOWTapeModel/
   '';
 
